2befd2309ef95eff94d378a373797319c1449df4
newbie-citizen <newbie.citizen@gmail.com>
2024-05-14 05:50:01 +0700

    .

--- package.js
+++ package.js
@@ -1,68 +1,86 @@
-const lib = require ("lib.min.js");
+/**
+ * xxx
+ *
+ * title
+ * description
+ * sub description
+ *
+ * xxx://xxx.xxx.xxx/xxx
+ */
 
 function express () {
 	return new express.app ();
 	}
 
+express.io = require ("express");
+express.body = {parser: require ("body-parser")}
+express.static = express.io.static;
+express.port = 3000;
+
+var lib = require ("lib.min.js");
+
+/**
+ * xxx
+ *
+ * title
+ * description
+ * sub description
+ *
+ * xxx://xxx.xxx.xxx/xxx
+ */
+
 express.app = class {
 	constructor () {
-		this.app = express.js ();
+		this.$app = express.io ();
 		this.client = new express.app.client (this);
 		}
-	use (... arg) {
-		this.app.use (... arg);
-		return this;
-		}
-	set (... arg) {
-		this.app.set (... arg);
-		return this;
-		}
-	get (path, context) {
-		this.app.get (express.path (path), context);
-		return this;
-		}
-	post (path, context) {
-		this.app.post (express.path (path), context);
-		return this;
-		}
-	listen (host, port, context) {
-		this.app.listen (port, host, context);
-		return this;
-		}
-	export ($) {
-		$.exports = this.app;
-		return this;
-		}
+	use (... context) { this.$app.use (... context); return this; }
+	set (... data) { this.$app.set (... data); return this; }
+	get (path, context) { this.$app.get (express.path (path), function (request, response, next) { context (request.io, response.io, next); }); return this; }
+	post (path, context) { this.$app.post (express.path (path), function (request, response, next) { context (request.io, response.io, next); }); return this; }
+	listen (host, port, context) { this.$app.listen (port, host, context); return this; }
 	html (dir) {
-		this.app.engine ("html", function (fp, option, context) {
-			lib.fs.api.readFile (fp, function (error, content) {
+		this.$app.engine ("html", function (fp, param, context) {
+			lib.fs.api.engine.readFile (fp, function (error, content) {
 				if (error) context (error);
 				var data = content.toString ();
-				data = lib.string (data).replace (option, {exclude: ["_locals", "settings", "cache"]});
+				data = data.to_replace (param, {exclude: ["_locals", "settings", "cache"]});
 				return context (null, data);
 				});
 			});
-		this.app.set ("views", dir);
-		this.app.set ("view engine", "html");
+		this.$app.set ("views", dir);
+		this.$app.set ("view engine", "html");
+		this.$app.use (function (request, response, next) {
+			response.public_html = dir;
+			next ();
+			});
+		return this;
+		}
+	export ($) {
+		$.exports = this.$app;
 		return this;
 		}
 	}
 
 express.app.client = class {
 	constructor (express) {
-		this.app = (this.express = express).app;
+		this.$app = (this.express = express).$app;
+		}
+	use (context) {
+		this.$app.use (function (request, response, next) {
+			context (request.io, response.io, next);
+			});
 		}
 	get (path, context) {
-		this.app.get (express.path (path), function (request, response, next) {
-			if (request.cross.origin) if (request.client.ip) context (request, response, next);
-			else response.error.cross.origin ();
-			else response.error.cross.origin ();
+		this.$app.get (express.path (path), function (request, response, next) {
+			if (request.io.cross.origin) context (request.io, response.io, next);
+			else response.io.error ("cross-origin");
 			});
 		}
 	post (path, context) {
-		this.app.post (express.path (path), function (request, response, next) {
-			if (request.cross.origin) context (request, response, next);
-			else response.error.cross.origin ();
+		this.$app.post (express.path (path), function (request, response, next) {
+			if (request.io.cross.origin) context (request.io, response.io, next);
+			else response.io.error ("cross-origin");
 			});
 		}
 	export ($) {
@@ -70,70 +88,142 @@ express.app.client = class {
 		}
 	}
 
+/**
+ * xxx
+ *
+ * title
+ * description
+ * sub description
+ *
+ * xxx://xxx.xxx.xxx/xxx
+ */
+
 express.request = function (app) {
 	return function (request, response, next) {
-		request.cross = {origin: false}
-		request.client = {id: ""}
-		request.parse_url = express.plugin.parse_url (app, request);
-		request.parse_url.param = function (param) { return request.params [param]; }
-		request.visitor = express.plugin.visitor (app, request);
+		request.io = new express.request.io (app, request, response, next);
 		next ();
 		}
 	}
 
-express.request.get = function (... url) { return lib.url.get (... url); }
-express.request.post = function (... url) { return lib.url.post (... url); }
-express.path = function (path) { return express.path.data [path] || path; }
+express.request.io = class {
+	constructor (app, request, response, next) {
+		this.app = app;
+		this.express = {request, response}
+		this.next = next;
+		this.__construct ();
+		}
+	__construct () {
+		var host = (this.header = this.express.request.headers) ["host"] || this.express.request.hostname;
+		var url = host + this.express.request.url;
+		this.url = lib.parse_url (url, {
+			protocol: this.header ["x-forwarded-proto"],
+			client: this.header ["origin"],
+			});
+		this.path = this.url.path;
+		this.cross = {origin: this.url.cross.origin}
+		this.client = {id: "", ip: "", ... this.url.client}
+		}
+	query (key) {
+		if (key) return this.express.request.query (key);
+		else return this.url.query;
+		}
+	param (key) {
+		return this.express.request.params [key];
+		}
+	}
+
+/**
+ * xxx
+ *
+ * title
+ * description
+ * sub description
+ *
+ * xxx://xxx.xxx.xxx/xxx
+ */
+
+express.response = function (app) {
+	return function (request, response, next) {
+		response.io = new express.response.io (app, request, response, next);
+		next ();
+		}
+	}
+
+express.response.io = class {
+	constructor (app, request, response, next) {
+		this.app = app;
+		this.express = {request, response}
+		this.request = request.io;
+		this.next = next;
+		this.__construct ();
+		}
+	__construct () {
+		//
+		}
+	status (status) {
+		if (status === "error:found") status = 404;
+		if (status === "error:forbidden") status = 403;
+		this.express.response.status (status);
+		return this;
+		}
+	send (... data) {
+		this.express.response.send (... data);
+		return this;
+		}
+	html (template, param) {
+		if (arguments.length > 1) this.express.response.render (template, param);
+		else this.express.response.render ("index", template);
+		}
+	file (file, option, context) {
+		if (file.startsWith ("/")) file = file.substr (1);
+		this.express.response.sendFile (file, {root: this.express.response.public_html, ... option}, function (error) {
+			if (error) this.express.response.status (404).send ("Error (404) Not Found : File");
+			if (context) context (error);
+			}.bind ({express: this.express}));
+		}
+	error (error) {
+		if (error === "cross-origin") this.express.response.status (403).send ("Error (403) Forbidden : Cross Origin");
+		}
+	}
+
+/**
+ * xxx
+ *
+ * title
+ * description
+ * sub description
+ *
+ * xxx://xxx.xxx.xxx/xxx
+ */
+
+express.path = function (path) {
+	return express.path.data [path] || path;
+	}
+
 express.path.data = {
 	"favorite.ico": "/favicon.ico",
 	}
 
-express.response = function () {
-	return function (request, response, next) {
-		response.error = function () {}
-		response.error.cross = function () {}
-		response.error.cross.origin = function () { response.status (403).send ("Forbidden : Cross Origin"); }
-		response.file = function (file, option, context) {
-			if (file.startsWith ("/")) file = file.substr (1);
-			response.sendFile (file, {root: response.folder, ... option}, function (error) {
-				if (error) response.status (404).send ("Error (404) : Not Found");
-				if (context) context (error);
-				});
-			}
-		response.html = function (template, param) {
-			if (arguments.length > 1) response.render (template, param);
-			else response.render ("index", template);
-			}
-		next ();
-		}
-	}
-
-express.js = require ("express");
-express.body = {parser: require ("body-parser")}
-express.static = express.js.static;
-express.port = 3000;
-express.require = function (json) { return "./" + json; }
-express.context = function (context) { return context || function () {} }
-
-express.client = function () {}
-express.client.header = function (app) {
-	return function (request, response, next) {
-		request.client.ip = request.headers ["x-client-ip"];
-		next ();
-		}
-	}
-express.client.port = 8000;
+/**
+ * xxx
+ *
+ * title
+ * description
+ * sub description
+ *
+ * xxx://xxx.xxx.xxx/xxx
+ */
 
 express.cross = function () {}
-express.cross.origin = function () { return express.cross.origin.api (); }
+express.cross.origin = function (app) { return express.cross.origin.api (); }
 express.cross.origin.api = require ("cors");
 express.cross.origin.access = function (app) {
 	return function (request, response, next) {
-		if (request.cross.origin) {
+		if (request.io.cross.origin) {
 			var co = app ["package.json"].cross.origin;
 			if (co === "*") next ();
-			else if (co.includes (request.client.host.name)) next ();
-			else response.error.cross.origin ();
+			else if (co.includes (request.io.client.base.name)) next ();
+			else response.io.error ("cross-origin");
 			}
 		else next ();
 		}
@@ -148,4 +238,65 @@ express.cross.origin.header = function (app) {
 		}
 	}
 
+/**
+ * xxx
+ *
+ * title
+ * description
+ * sub description
+ *
+ * xxx://xxx.xxx.xxx/xxx
+ */
+
+express.client = function () {}
+express.client.port = 8000;
+express.client.header = function (app) {
+	return function (request, response, next) {
+		request.client.ip = request.header ["x-client-ip"];
+		next ();
+		}
+	}
+
+/**
+ * xxx
+ *
+ * title
+ * description
+ * sub description
+ *
+ * xxx://xxx.xxx.xxx/xxx
+ */
+
+express.visitor = function (app) {
+	return function (request, response, next) {
+		var ip = request.header ["x-client-ip"] || request.header ["cf-connecting-ip"] || request.header ["x-forwarded-for"] || request.header ["x-real-ip"];
+		var option = {
+			country_code: request.header ["cf-ipcountry"] || request.header ["x-vercel-ip-country"],
+			region_code: request.header ["x-vercel-ip-country-region"],
+			city: decodeURI (request.header ["x-vercel-ip-city"] || ""),
+			timezone: request.header ["x-vercel-ip-timezone"],
+			latitude: request.header ["x-vercel-ip-latitude"],
+			longitude: request.header ["x-vercel-ip-longitude"],
+			}
+		request.visitor = lib.ip.parse (ip, option);
+		next ();
+		}
+	}
+
+/**
+ * xxx
+ *
+ * title
+ * description
+ * sub description
+ *
+ * xxx://xxx.xxx.xxx/xxx
+ */
+
 module.exports = exports = express;
+
+/**
+ * the end
+ *
+ * xxx://xxx.xxx.xxx/xxx
+ */
